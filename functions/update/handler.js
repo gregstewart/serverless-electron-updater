b813c48198cf4adc792a6ae394d757d22a756fd3
Greg Stewart <gregs@tcias.co.uk>
2016-06-27 22:43:36 +0100

    Add ES6 support

--- functions/update/handler.js
+++ functions/update/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.handler = function(event, context, cb) {
+export default (event, context, cb) => {
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
   });
